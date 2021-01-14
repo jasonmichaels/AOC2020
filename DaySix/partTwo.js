b025773892ba2ac3705fdb3715f3adeaea78d06b
Jason White <jasonmichaelwhite@gmail.com>
2021-01-13 22:48:37 -0500

    UPDATED: partTwo

--- DaySix/partTwo.js
+++ DaySix/partTwo.js
@@ -3,15 +3,13 @@ const fs = require('fs');
 const testData = fs.readFileSync('test.txt', { encoding: 'UTF-8' }).split('\n\n').filter(Boolean);
 const allData = fs.readFileSync('data.txt', { encoding: 'UTF-8' }).split('\n\n').filter(Boolean);
 
-const getCount = data => {
-  return data.reduce((acc, curr) =>
-    acc + [...new Set([...curr.replace(/\n/g, '')])] // get unique letters
-    .filter(c => curr.split('\n')
-      .filter(Boolean)
-      .every(d => d.includes(c))) // every one should contain the same letter, else ditch it
-    .length,
-    0);
-};
+const getCount = data => data.reduce((acc, curr) =>
+  acc + [...new Set([...curr.replace(/\n/g, '')])]
+  .filter(c => curr.split('\n')
+    .filter(Boolean)
+    .every(d => d.includes(c)))
+  .length,
+  0);
 
 console.log(getCount(testData)); // 6
 console.log(getCount(allData)); // 3125
